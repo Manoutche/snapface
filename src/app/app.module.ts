6d8305bc6ad104a9736e8e236018359c51d80fd6
Manoutche <67541888+Manoutche@users.noreply.github.com>
2024-03-28 16:31:20 +0000

    auth

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,15 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
-import { LandingPageComponent } from './landing-page/landing-page.component';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from "@angular/common/http";
 import { CoreModule } from './core/core.module';
 import { AuthModule } from './auth/auth.module';
+import { LandingPageModule } from './landing-page/landing-page.module';
+import { FaceSnapsModule } from './face-snaps/face-snaps.module';
 @NgModule({
   declarations: [
     AppComponent,
-    LandingPageComponent,
   ],
   imports: [
     BrowserModule,
@@ -18,6 +18,8 @@ import { AuthModule } from './auth/auth.module';
     FormsModule, /** form module */
     HttpClientModule,
     CoreModule,
+    LandingPageModule,
+    FaceSnapsModule,
     AuthModule
   ],
 
